f1995578c327e19cfaef319d40b66bbf4493c012
Harutaka Kawamura <hkawamura0130@gmail.com>
2020-07-31 23:07:29 +0900

    Read config from input (#3)
    * Read config from input
    
    * build
    
    * fix
    
    * catch

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
+import * as core from '@actions/core';
+
 import { readConfig } from './utils';
 
 async function main(): Promise<void> {
-  const config = readConfig('./.github/.label-notifier.json');
+  const configPath = core.getInput('config-path', { required: false });
+  const config = readConfig(configPath);
   console.log(config);
 }
 
-main();
+main().catch(err => {
+  throw err;
+});
