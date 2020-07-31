599ea118af1c2a1878d4492eadef55c4ea298b16
Harutaka Kawamura <hkawamura0130@gmail.com>
2020-07-31 22:25:43 +0900

    Add config file (#2)
    * Add readConfig
    
    * Add test-run
    
    * Simplify lint command

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,5 +1,5 @@
 module.exports = {
-  printWidth: 80,
+  printWidth: 90,
   trailingComma: 'all',
   tabWidth: 2,
   semi: true,
--- dist/index.js
+++ dist/index.js
@@ -44,18 +44,61 @@ module.exports =
 /******/ ({
 
 /***/ 198:
-/***/ (function(__unusedmodule, exports) {
+/***/ (function(__unusedmodule, exports, __webpack_require__) {
 
 "use strict";
 
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.sample = void 0;
-function sample() {
-    return 'foo';
+const utils_1 = __webpack_require__(611);
+function main() {
+    return __awaiter(this, void 0, void 0, function* () {
+        const config = utils_1.readConfig('./.github/.label-notifier.json');
+        console.log(config);
+    });
 }
-exports.sample = sample;
-sample();
+main();
 
+
+/***/ }),
+
+/***/ 611:
+/***/ (function(__unusedmodule, exports, __webpack_require__) {
+
+"use strict";
+
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.readConfig = exports.readFile = void 0;
+const fs_1 = __importDefault(__webpack_require__(747));
+function readFile(path) {
+    return fs_1.default.readFileSync(path, 'utf8');
+}
+exports.readFile = readFile;
+function readConfig(path) {
+    const configStr = readFile(path);
+    const config = JSON.parse(configStr);
+    return config;
+}
+exports.readConfig = readConfig;
+
+
+/***/ }),
+
+/***/ 747:
+/***/ (function(module) {
+
+module.exports = require("fs");
 
 /***/ })
 
