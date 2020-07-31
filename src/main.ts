dc96c27ed921918dff9a3a7b40c142320f388f42
harupy <17039389+harupy@users.noreply.github.com>
2020-07-31 23:22:19 +0900

    Add notify workflow
    Signed-off-by: harupy <17039389+harupy@users.noreply.github.com>

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,18 @@
 import * as core from '@actions/core';
+import * as github from '@actions/github';
 
 import { readConfig } from './utils';
 
 async function main(): Promise<void> {
+  const token = core.getInput('github-token', { required: true });
   const configPath = core.getInput('config-path', { required: false });
+
+  const octokit = github.getOctokit(token);
   const config = readConfig(configPath);
+
+  console.log(github.context);
   console.log(config);
+  console.log(octokit);
 }
 
 main().catch(err => {
